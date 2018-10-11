f898b3f2567fea02f9a8bbeda8b7b0e192ac8ffb
stumash <stuart.mashaal@gmail.com>
2018-10-10 23:18:19 -0400

    fix dining philosopher scripts

--- 01/code/scripts/buildAndRun_DiningPhilosophers.sh
+++ 01/code/scripts/buildAndRun_DiningPhilosophers.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
 THIS_DIR="$(dirname "$(readlink -f "$0")")"
+cd "${THIS_DIR}"
 
 ./build_DiningPhilosophers.sh && ./run_DiningPhilosophers.sh
--- 01/code/scripts/run_DiningPhilosophers.sh
+++ 01/code/scripts/run_DiningPhilosophers.sh
@@ -8,4 +8,6 @@ then
     echo "not found: build/ca/mcgill/ecse420/a1/diningPhilosophers/DiningPhilosophers.class"
 fi
 
+cd "build"
+
 java ca.mcgill.ecse420.a1.diningPhilosophers.DiningPhilosophers
